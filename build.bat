830936152c9f57842d9bdf1039c5f2b2c3eb8dbd
ymmtmdk <ymmtmdk@gmail.com>
2025-11-16 00:01:28 +0900

    m

--- build.bat
+++ build.bat
@@ -3,11 +3,8 @@
 REM Call vcvars64.bat to set up the Visual Studio environment
 call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-REM Define the CMAKE variable
-set "CMAKE=C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe"
-
 REM Run CMake to configure the project
-"%CMAKE%" -S . -B build -G "Ninja" -DCMAKE_TOOLCHAIN_FILE="c:\Users\tuftu\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build -G "Ninja" -DCMAKE_TOOLCHAIN_FILE="c:\Users\tuftu\vcpkg\scripts\buildsystems\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release
 
 REM Run CMake to build the project
-"%CMAKE%" --build build --config Release --parallel
+cmake --build build --config Release --parallel
